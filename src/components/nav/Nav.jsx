89767bf756eb2bbd748670435f7301e598b9ca18
Anurag Rawat <anuragrawat88393@gmail.com>
2024-03-31 00:18:24 +0530

    final

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
             <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
             <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
             <a href='#skills' onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><GiSkills /></a>
-            <a href='#projects' onClick={() => setActiveNav('#projects')} className={activeNav === '#my_resume' ? 'active' : ''}><MdOutlineContactPage /></a>
+            <a href='#my_resume' onClick={() => setActiveNav('#my_resume')} className={activeNav === '#my_resume' ? 'active' : ''}><MdOutlineContactPage /></a>
             <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
         </nav>
     )
